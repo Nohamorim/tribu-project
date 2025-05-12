7e43fe8f16c8800afbd4948c3f0d85c8a04c5852
Nohamorim <myamorimads2023@gmail.com>
2025-05-12 15:14:22 -0300

    feat: Inicializar projeto tribuy com estrutura básica

---
+++ usuarios/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Cliente(models.Model):
+    class Sexo(models.TextChoices):
+        MASCULINO = 'M', 'Masculino'
+        FEMININO = 'F', 'Feminino'
+
+    nome = models.CharField("Nome completo", max_length=100)
+    data_nascimento = models.DateField("Data de nascimento", blank=True, null=True)
+    sexo = models.CharField("Sexo", max_length=1, choices=Sexo.choices)
+    email = models.EmailField("E-mail", blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
