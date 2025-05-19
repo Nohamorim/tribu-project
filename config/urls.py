8cde0887688c94aa08c51690728f0a627087b818
Nohamorim <myamorimads2023@gmail.com>
2025-05-19 16:16:49 -0300

    feat: criação swagger

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 
+# Configurações do Swagger
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Informações do Swagger
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API Tribu",
+      default_version='v1',
+      description="Controle de cadastro de usuários",
+      terms_of_service="#",
+      contact=openapi.Contact(email="myamorimads2023@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 def home(request):
     return HttpResponse("Bem-vindo à página inicial!")
 
@@ -9,4 +28,5 @@ urlpatterns = [
     path('', home),  # Rota para a raiz /
     path('admin/', admin.site.urls),
     path('api/', include('usuarios.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
--- usuarios/serializers.py
+++ usuarios/serializers.py
@@ -5,3 +5,9 @@ class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'  # Isso inclui todos os campos do modelo Cliente
+
+class ClienteFilterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = ['id', 'nome']  # Inclui apenas os campos id e nome para o filtro
+#SERIALIZERS filter
